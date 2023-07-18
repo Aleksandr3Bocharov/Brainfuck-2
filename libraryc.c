622e3ac78d0d1d00259c1aab7b9b11774979f5ca
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2023-07-18 16:08:11 +0300

    Add files via upload

--- libraryc.c
+++ libraryc.c
@@ -86,6 +86,27 @@ R05_DEFINE_ENTRY_FUNCTION(GetChar, "GetChar") {
 
 /*
 
+<GTKInit> ==
+
+*/
+R05_DEFINE_ENTRY_FUNCTION(GTKInit, "GTKInit") {
+  struct r05_node *callee = arg_begin->next;
+
+  if (callee->next != arg_end)
+  {
+    r05_recognition_impossible();
+  }
+
+  r05_reset_allocator();
+
+  gtk_init(0, NULL);
+
+  r05_splice_from_freelist(arg_begin);
+  r05_splice_to_freelist(arg_begin, arg_end);
+}
+
+/*
+
 <OpenFileDialog> == e.bfFileName
 
 e.bfFileName ::= s.CHAR*
@@ -136,8 +157,9 @@ R05_DEFINE_ENTRY_FUNCTION(OpenFileDialog, "OpenFileDialog") {
 
 /*
 
-<SaveFileDialog e.FileName> == e.FileName
+<SaveFileDialog e.iFileName> == e.FileName
 
+e.iFileName ::= s.CHAR+
 e.FileName ::= s.CHAR*
 
 */
@@ -199,6 +221,62 @@ R05_DEFINE_ENTRY_FUNCTION(SaveFileDialog, "SaveFileDialog") {
     g_free(filename);
   }
 
+  gtk_widget_destroy(dialog);
+
+  r05_splice_from_freelist(arg_begin);
+  r05_splice_to_freelist(arg_begin, arg_end);
+}
+
+/*
+
+<MessageBox e.Message> ==
+
+e.Message ::= s.CHAR+
+
+*/
+R05_DEFINE_ENTRY_FUNCTION(MessageBox, "MessageBox") {
+  struct r05_node *msg_b, *msg_e;
+
+#define MESSAGE_MAX 1024
+  char message[MESSAGE_MAX + 1];
+  size_t message_len;
+
+  /* сопоставление с образцом */
+  r05_prepare_argument(&msg_b, &msg_e, arg_begin, arg_end);
+  message_len = r05_read_chars(message, MESSAGE_MAX, &msg_b, &msg_e);
+  message[message_len] = '\0';
+
+  if (message_len == 0)
+    r05_recognition_impossible();
+
+  if (! r05_empty_seq(msg_b, msg_e))
+  {
+    struct r05_node *p = msg_b;
+    
+    while (p != msg_e->next && p->tag == R05_DATATAG_CHAR)
+      p = p->next;
+
+    if (p == msg_e->next)
+      r05_builtin_error("very long message");
+    else
+      r05_recognition_impossible();
+  }
+
+#undef MESSAGE_MAX
+
+  r05_reset_allocator();
+  
+  GtkWidget *dialog;
+  GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
+  dialog = gtk_message_dialog_new(NULL,
+                                  flags,
+                                  GTK_MESSAGE_INFO,
+                                  GTK_BUTTONS_OK,
+                                  "%s",
+                                  message);
+  
+  gtk_dialog_run(GTK_DIALOG(dialog));
+  
   gtk_widget_destroy(dialog);
 
   r05_splice_from_freelist(arg_begin);
